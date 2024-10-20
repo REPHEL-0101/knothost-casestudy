638c68408767292b07e841e2c1774f35a2f0e363
REPHEL-0101 <97841186+REPHEL-0101@users.noreply.github.com>
2024-10-20 21:34:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Knothost Construction Company - Crafting excellence, Building your future.">
     <title>Knothost Construction Company</title>
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
